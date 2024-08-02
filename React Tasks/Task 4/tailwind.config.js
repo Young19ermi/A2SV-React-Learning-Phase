a14747d60fce63a0d5fbcfd4cb343a9c661ea7ee
Ermias Seleshi Endale <111576904+Young19ermi@users.noreply.github.com>
2024-08-02 08:32:57 -0700

    Add files via upload

---
+++ React Tasks/Task 4/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to your file extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
